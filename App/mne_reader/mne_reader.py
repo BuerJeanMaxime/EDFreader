d8b2f7277004fbb6b8d179cdd71b721349b23531
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-01 10:07:38 +0100

    feat(mne_reader): use highlevel to compute aih

--- App/mne_reader/mne_reader.py
+++ App/mne_reader/mne_reader.py
@@ -1,10 +1,16 @@
 import mne
 from mne.io.edf.edf import RawEDF
+from pyedflib import highlevel
 
 
 class MNEReader:
 
     @staticmethod
-    def read_edf_file(path):
+    def read_edf_file_mne(path):
         raw_edf: RawEDF = mne.io.read_raw_edf(path)
         return raw_edf
+
+    @staticmethod
+    def read_edf_file_pyedflib(path):
+        signals, signal_headers, header = highlevel.read_edf(path)
+        return signals, signal_headers, header
