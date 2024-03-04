74990e4ba62b82fbd92de21687d08d6214cb5036
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 09:32:56 +0100

    feat(typing): put types in metrics_measure and create custom types for numpy array

---
+++ App/custom_types.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+FloatArray = np.ndarray[float]
+IntArray = np.ndarray[int]
--- App/metrics_measure/metrics_measure.py
+++ App/metrics_measure/metrics_measure.py
@@ -1,5 +1,6 @@
 from pyedflib import highlevel
 
+from App.custom_types import FloatArray, IntArray
 from App.mne_reader.mne_reader import MNEReader
 
 
@@ -8,23 +9,23 @@ class MetricsMeasure:
     @staticmethod
     def compute_AIH(path):
         signals, signal_headers, header = MNEReader.read_edf_file_pyedflib(path)
-        airflow_signal = signals[0]
+        airflow_signal: FloatArray = signals[0]
 
-        apnea_threshold = 0.5  # Adjust according to your signal characteristics
-        hypopnea_threshold = 0.3  # Adjust according to your signal characteristics
-        sample_frequency = signal_headers[0]['sample_rate']
-        num_samples = len(signals[0])
+        apnea_threshold: float = 0.5  # Adjust according to your signal characteristics
+        hypopnea_threshold: float = 0.3  # Adjust according to your signal characteristics
+        sample_frequency: float = signal_headers[0]['sample_rate']
+        num_samples: int = len(signals[0])
 
         # Calculate the duration
-        duration_seconds = num_samples / sample_frequency
+        duration_seconds: float = num_samples / sample_frequency
         # Detect apneas and hypopneas
-        apnea_events = (airflow_signal < apnea_threshold).astype(int)
-        hypopnea_events = ((airflow_signal >= apnea_threshold) & (airflow_signal < hypopnea_threshold)).astype(int)
+        apnea_events: IntArray = (airflow_signal < apnea_threshold).astype(int)
+        hypopnea_events: float = ((airflow_signal >= apnea_threshold) & (airflow_signal < hypopnea_threshold)).astype(int)
 
-        total_apneas = apnea_events.sum()
-        total_hypopneas = hypopnea_events.sum()
-        total_sleep_time_hours = duration_seconds / 3600  # Convert seconds to hours
+        total_apneas: int = apnea_events.sum()
+        total_hypopneas: int = hypopnea_events.sum()
+        total_sleep_time_hours: float = duration_seconds / 3600  # Convert seconds to hours
 
-        ahi = (total_apneas + total_hypopneas) / total_sleep_time_hours * 60
+        ahi: float = (total_apneas + total_hypopneas) / total_sleep_time_hours * 60
         return ahi
 
